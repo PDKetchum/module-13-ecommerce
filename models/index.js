214cec97a55003fd7998bb1502080011ae189ccc
PDKetchum <pxiong265@gmail.com>
2022-10-04 00:09:00 -0500

    start product route function

--- models/index.js
+++ models/index.js
@@ -4,17 +4,13 @@ const Category = require("./Category");
 const Tag = require("./Tag");
 const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: "category_id",
 });
 
-// Categories have many Products
 Category.hasMany(Product, {
   foreignKey: "category_id",
 });
-
-// Products belongToMany Tags (through ProductTag)
 
 Product.belongsToMany(Tag, {
   through: {
@@ -23,7 +19,6 @@ Product.belongsToMany(Tag, {
   },
 });
 
-// Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: {
     model: ProductTag,
