614cd25f4ab44d7de415209c0ed53ab656792dd9
PDKetchum <pxiong265@gmail.com>
2022-10-05 00:07:03 -0500

    Fix bugs

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const routes = require("./routes");
 // import sequelize connection
 const sequelize = require("./config/connection");
+const seeds = require("./seeds");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
